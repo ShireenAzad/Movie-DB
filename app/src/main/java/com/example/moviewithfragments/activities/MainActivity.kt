4382501ecfe997c70f45deb9f49614970907c6ca
shireen <syed.shireen@everest.engineering>
2022-03-28 23:05:39 +0530

    activities added.

--- app/src/main/java/com/example/moviewithfragments/activities/MainActivity.kt
+++ app/src/main/java/com/example/moviewithfragments/activities/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.moviewithfragments
+package com.example.moviewithfragments.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
---
+++ app/src/main/java/com/example/moviewithfragments/activities/MovieDetailsActivity.kt
@@ -0,0 +1,29 @@
+package com.example.moviewithfragments.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.example.moviewithfragments.api.MovieApiUtilities
+import com.example.moviewithfragments.model.Movies
+
+class MovieDetailsActivity : AppCompatActivity() {
+    private lateinit var activityMovieDetailsBinding: ActivityMovieDetailsBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityMovieDetailsBinding = ActivityMovieDetailsBinding.inflate(layoutInflater)
+        setContentView(activityMovieDetailsBinding.root)
+        if(intent.hasExtra("movie")){
+            val movieModel = intent.getParcelableExtra<Movies>("movie")
+            activityMovieDetailsBinding.title.text=movieModel?.title.toString()
+            activityMovieDetailsBinding.overview.text=movieModel?.overview.toString()
+            if (movieModel != null) {
+                activityMovieDetailsBinding.ratingBar.rating= ((movieModel.voteAverage)/2).toFloat()
+            }
+
+            Glide.with(this).load(MovieApiUtilities.IMAGE_URI+movieModel?.posterPath)
+                .into(activityMovieDetailsBinding.imageView)
+        }
+
+    }
+
+}
