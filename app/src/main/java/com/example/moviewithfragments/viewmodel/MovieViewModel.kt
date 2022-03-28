976741312a97e313fd789ea95bb407f5546b961a
shireen <syed.shireen@everest.engineering>
2022-03-29 00:18:47 +0530

    View model updated to enable rooms work in offline mode.

--- app/src/main/java/com/example/moviewithfragments/viewmodel/MovieViewModel.kt
+++ app/src/main/java/com/example/moviewithfragments/viewmodel/MovieViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.moviewithfragments.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -11,7 +10,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import retrofit2.HttpException
 
 class MovieViewModel(
     private var movieRepository: MovieRepository,
@@ -21,14 +19,13 @@ class MovieViewModel(
     val movies: LiveData<List<Movies>> = _movies
     fun getMovies(): List<Movies>? {
         CoroutineScope(Dispatchers.IO).launch {
-            val response = movieRepository.getAllMovies()
             withContext(Dispatchers.Main) {
                 try {
+                    val response = movieRepository.getAllMovies()
                     _movies.postValue(response!!)
                     moviesDatabase.moviesDao().insertMovies(response)
                 } catch (e: Throwable) {
-                    moviesDatabase.moviesDao().getMovies()
-                    Log.v("Ooops", "Data not found" + e.message)
+                    _movies.postValue(moviesDatabase.moviesDao().getMovies())
                 }
             }
         }
@@ -45,8 +42,8 @@ class MovieViewModel(
                     moviesDatabase.moviesDao().insertMovies(response)
 
                 } catch (e: Throwable) {
-                    moviesDatabase.moviesDao().getMovies()
-                    Log.v("Ooops", "Data not found")
+                    _movies.postValue(moviesDatabase.moviesDao().getMovies())
+
                 }
             }
 
@@ -59,21 +56,11 @@ class MovieViewModel(
             val response = movieRepository.searchMovies(query)
             withContext(Dispatchers.Main) {
                 try {
-                    if (response != null) {
-                        if (response.isNotEmpty()) {
-                            _movies.value = response!!
-                            moviesDatabase.moviesDao().insertMovies(response)
-                            Log.v("Popular", "List Check" + movies.value)
-                            //Do something with response e.g show to the UI.
-                        } else {
-                            Log.v("Error", "Error: ${response}")
-                        }
-                    }
-                } catch (e: HttpException) {
-                    Log.v("Exception", "No  ${e.message}")
-                } catch (e: Throwable) {
+                    _movies.value = response!!
+                    moviesDatabase.moviesDao().insertMovies(response)
 
-                    Log.v("Ooops", "Data not found")
+                } catch (e: Throwable) {
+                    _movies.postValue(moviesDatabase.moviesDao().getMovies())
                 }
             }
 
