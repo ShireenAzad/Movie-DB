f4420677aa985fef912185aed79709e02e956945
shireen <syed.shireen@everest.engineering>
2022-03-29 01:56:03 +0530

    Search function ui designed.

--- app/src/main/java/com/example/moviewithfragments/activities/MainActivity.kt
+++ app/src/main/java/com/example/moviewithfragments/activities/MainActivity.kt
@@ -1,7 +1,15 @@
 package com.example.moviewithfragments.activities
 
+import android.app.SearchManager
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuItemCompat
+import com.example.moviewithfragments.R
 import com.example.moviewithfragments.adapters.MainAdapter
 import com.example.moviewithfragments.databinding.ActivityMainBinding
 import com.google.android.material.tabs.TabLayoutMediator
@@ -21,5 +29,35 @@ class MainActivity : AppCompatActivity() {
             tabLayout, viewPager2
         ) { tab, position -> tab.text = tabTitles[position] }.attach()
 
+
+}
+override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    menuInflater.inflate(R.menu.main_menu, menu)
+
+    // Retrieve the SearchView and plug it into SearchManager
+    val searchView = MenuItemCompat.getActionView(menu.findItem(R.id.search)) as SearchView
+    val searchManager = getSystemService(AppCompatActivity.SEARCH_SERVICE) as SearchManager
+    searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+    searchView.setOnClickListener(object : View.OnClickListener{
+        override fun onClick(v: View?) {
+            searchView.onActionViewExpanded()
+        }
+    })
+    return true
+}
+override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    return when (item.getItemId()) {
+        R.id.search -> {
+            finish()
+            val intent = Intent(this@MainActivity, SearchActivity::class.java)
+            startActivity(intent)
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 }
+}
+
+
+
+
