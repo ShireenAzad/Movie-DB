210281db81304f57c1b262e184a3d2665afc9036
shireen <syed.shireen@everest.engineering>
2022-03-28 22:54:53 +0530

    Tablayout added.

--- app/src/main/java/com/example/moviewithfragments/MainActivity.kt
+++ app/src/main/java/com/example/moviewithfragments/MainActivity.kt
@@ -2,13 +2,24 @@ package com.example.moviewithfragments
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.moviewithfragments.adapters.MainAdapter
 import com.example.moviewithfragments.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
-class MainActivity:AppCompatActivity()   {
+class MainActivity :AppCompatActivity() {
     private lateinit var activityMainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState)
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(activityMainBinding.root)}
+        setContentView(activityMainBinding.root)
+        val tabLayout = activityMainBinding.tabs
+        val viewPager2 = activityMainBinding.viewPager
+        val adapter = MainAdapter(this)
+        viewPager2.adapter = adapter
+        val tabTitles = arrayOf("Popular Movies", "Latest Movies")
+        TabLayoutMediator(
+            tabLayout, viewPager2
+        ) { tab, position -> tab.text = tabTitles[position] }.attach()
 
+    }
 }
