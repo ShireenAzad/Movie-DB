b74de4b589e05e050a90170205b21ac0e7349fa4
shireen <syed.shireen@everest.engineering>
2022-03-29 23:19:08 +0530

    Serailised names removed from entity class.

--- app/src/main/java/com/example/moviewithfragments/model/Movies.kt
+++ app/src/main/java/com/example/moviewithfragments/model/Movies.kt
@@ -3,24 +3,18 @@ package com.example.moviewithfragments.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.google.gson.annotations.SerializedName
 import java.util.*
+
 //Entity class for db model
 @Entity(tableName = "movies")
 data class Movies(
-    @SerializedName("id")
     val id: Int,
-    @SerializedName("overview")
     val overview: String,
-    @SerializedName("poster_path")
     val posterPath: String,
-    @SerializedName("release_date")
     val releaseDate: Date,
     @PrimaryKey
-    @SerializedName("title")
     val title: String,
-    @SerializedName("vote_average")
     val voteAverage: Double,
     @ColumnInfo
-    var movieType:String?=""
+    var movieType: String? = ""
 )
