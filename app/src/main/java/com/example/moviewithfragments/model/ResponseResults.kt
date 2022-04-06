000dd848a196e13d18d6b499ff8b8da4b9df43b9
shireen <syed.shireen@everest.engineering>
2022-04-06 14:58:34 +0530

    data and error constants removed from constructor of Response Results.

--- app/src/main/java/com/example/moviewithfragments/model/ResponseResults.kt
+++ app/src/main/java/com/example/moviewithfragments/model/ResponseResults.kt
@@ -1,10 +1,7 @@
 package com.example.moviewithfragments.model
 
-sealed class ResponseResults<T>(
-    val data: T? = null,
-    val error: String? = null
-) {
-    class Success<T>(data: T? = null) : ResponseResults<T>(data = data)
-    class Failure<T>(error: String) : ResponseResults<T>(error = error)
+sealed class ResponseResults<T>() {
+    class Success<T>(val data: T? = null) : ResponseResults<T>()
+    class Failure<T>(val error: String) : ResponseResults<T>()
     class Loading<T>() : ResponseResults<T>()
 }
